465e3fb6a76721ec25ae08d5ae8dfd8ea3e4444f
David Daniel <davedan7@gmail.com>
2015-02-19 16:20:32 -0700

    TDD, The first real attempt

---
+++ chisel_test.rb
@@ -0,0 +1,16 @@
+require 'minitest'
+require 'minitest/autorun'
+require './chisel'
+
+class ChiselTest < Minitest::Test
+
+	def test_it_can_take_an_input 
+		parser = Chisel.new
+	end
+
+	def test_it_outputs_a_file
+		parser = Chisel.new
+		assert_equal
+	end
+
+end
